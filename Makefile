8eee958bfc110c74bec1a50a54ad9b70a9148c91
Vincent <mrccniv@gmail.com>
2021-11-21 17:14:10 +0100

    first commit

--- Makefile
+++ Makefile
@@ -11,16 +11,16 @@ DATA=data
 SAVE=save
 
 #arguments pour trouver les éléments dans les dossiers
-SRCS=$(wildcard $(SRC)/*.c)
-HEADS=$(wildcard $(SRC)/*.h)
-OBJS=$(patsubst $(SRC)/%.c, $(OBJ)/%.o, $(SRCS))
+SRCS=$(wildcard $(SRC)\*.c)
+HEADS=$(wildcard $(SRC)\*.h)
+OBJS=$(patsubst $(SRC)\%.c, $(OBJ)\%.o, $(SRCS))
 
-RESTORESRC=$(wildcard $(SAVE)/*.c)
-RESTOREHEAD=$(wildcard $(SAVE)/*.h)
+RESTORESRC=$(wildcard $(SAVE)\*.c)
+RESTOREHEAD=$(wildcard $(SAVE)\*.h)
 
 #dossier pour l'executable
 BINDIR=bin
-BIN=$(BINDIR)/main
+BIN=$(BINDIR)\main
 
 #varilable pour l'archivage
 nameArchive=barre_romain
@@ -31,7 +31,7 @@ all:$(BIN)
 $(BIN): $(OBJS)
 	$(CC) $(CFLAGS) $(OBJS) -o $@
 
-$(OBJ)/%.o: $(SRC)/%.c
+$(OBJ)\%.o: $(SRC)\%.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
 .PHONY: clean save restore give
@@ -45,10 +45,10 @@ restore:
 	@echo Sauvegarde restaure.
 
 give:
-	@mkdir $(nameArchive) $(nameArchive)/$(SRC) $(nameArchive)/$(SAVE) $(nameArchive)/$(BINDIR) $(nameArchive)/$(OBJ) $(nameArchive)/$(DATA) || echo Fichier deja existant.
-	@$(CP) $(RESTORESRC) $(RESTOREHEAD) $(nameArchive)/$(SRC) || echo Rien a copier.
-	@$(CP) Makefile $(nameArchive)/
+	@mkdir $(nameArchive) $(nameArchive)\$(SRC) $(nameArchive)\$(SAVE) $(nameArchive)\$(BINDIR) $(nameArchive)\$(OBJ) $(nameArchive)\$(DATA) || echo Fichier deja existant.
+	@$(CP) $(RESTORESRC) $(RESTOREHEAD) $(nameArchive)\$(SRC) || echo Rien a copier.
+	@$(CP) Makefile $(nameArchive)\
 
 clean:
-	$(RM) $(BINDIR)/* $(OBJ)/* || echo Rien a supprimer
+	$(RM) $(BINDIR)\* $(OBJ)\* || echo Rien a supprimer
 	$(RM) $(nameArchive) || echo Rien a supprimer
--- src/main.c
+++ src/main.c
@@ -1,34 +1,47 @@
 #include "header.h"
 
-int existeFichier(char* nomFichier){
-	FILE* file = fopen(nomFichier,"r");
-	
-	if(file == NULL){
+int existeFichier(char *nomFichier)
+{
+	FILE *file = fopen(nomFichier, "r");
+
+	if (file == NULL)
+	{
 		return 0;
-	}else{
+	}
+	else
+	{
 		fclose(file);
 		return 1;
 	}
 }
 
-int freqChar(char carac, char* nomFichier){
-	
-	FILE* file;
-	
+int freqChar(char carac, char *nomFichier)
+{
+
+	FILE *file;
+
 	int compteur = 0;
 	char val;
-	
-	if(!existeFichier(nomFichier)){
+
+	if (!existeFichier(nomFichier))
+	{
 		printf("\n*Le fichier %s n existe pas.*\n", nomFichier);
 		exit(-1);
-	}else{
-		if((file=fopen(nomFichier, "r"))==NULL){
-			printf("Erreur lors de l ouverture du fichier %s.",nomFichier);
+	}
+	else
+	{
+		if ((file = fopen(nomFichier, "r")) == NULL)
+		{
+			printf("Erreur lors de l ouverture du fichier %s.", nomFichier);
 			exit(-1);
-		}else{
+		}
+		else
+		{
 			rewind(file);
-			while((val = fgetc(file))!=EOF){
-				if(val == carac){
+			while ((val = fgetc(file)) != EOF)
+			{
+				if (val == carac)
+				{
 					compteur++;
 				}
 			}
@@ -37,136 +50,166 @@ int freqChar(char carac, char* nomFichier){
 			return compteur;
 		}
 	}
-	
 }
 
-listeChar* initialisationListe(void){
-    listeChar* tabCarac = (listeChar*) malloc(sizeof(listeChar*));
+listeChar *initialisationListe(void)
+{
+	listeChar *tabCarac = (listeChar *)malloc(sizeof(listeChar *));
 
-    if(tabCarac == NULL)
-    {
-        exit(-1);
-    }
+	if (tabCarac == NULL)
+	{
+		exit(-1);
+	}
 
-    return tabCarac;
+	return tabCarac;
 }
 
-void ajoutTabFin(char car, char* nomFichier, listeChar* liste){
-	Caractere* nCarac;
-	
-	if((nCarac = (Caractere*) malloc(sizeof(Caractere))) == NULL){
-        exit(-1);
-    }
-    
-	Caractere* current = liste->premier;
-    if(current == NULL){
-        nCarac->suivant = NULL;
-        nCarac->carac = car;
+void ajoutTabFin(char car, char *nomFichier, listeChar *liste)
+{
+	Caractere *nCarac;
+
+	if ((nCarac = (Caractere *)malloc(sizeof(Caractere))) == NULL)
+	{
+		exit(-1);
+	}
+
+	Caractere *current = liste->premier;
+	if (current == NULL)
+	{
+		nCarac->suivant = NULL;
+		nCarac->carac = car;
 		nCarac->frequence = freqChar(car, nomFichier);
-        liste->premier = nCarac;
-    }else{
-        while (current->suivant != NULL)
-        {
-            current = current->suivant;
-        }
-        nCarac->suivant = NULL;
-        nCarac->carac = car;
+		liste->premier = nCarac;
+	}
+	else
+	{
+		while (current->suivant != NULL)
+		{
+			current = current->suivant;
+		}
+		nCarac->suivant = NULL;
+		nCarac->carac = car;
 		nCarac->frequence = freqChar(car, nomFichier);
-        current->suivant = nCarac;
-    }
-
+		current->suivant = nCarac;
+	}
 }
 
-Caractere* ajoutTab(char car, char* nomFichier, listeChar* liste){
-	Caractere* nCarac;
-	
-	if((nCarac = (Caractere*) malloc(sizeof(Caractere))) == NULL){
-        exit(-1);
-    }
+Caractere *ajoutTab(char car, char *nomFichier, listeChar *liste)
+{
+	Caractere *nCarac;
 
-    nCarac->suivant = NULL;
-    nCarac->carac = car;
+	if ((nCarac = (Caractere *)malloc(sizeof(Caractere))) == NULL)
+	{
+		exit(-1);
+	}
+
+	nCarac->suivant = NULL;
+	nCarac->carac = car;
 	nCarac->frequence = freqChar(car, nomFichier);
 
 	return nCarac;
-
 }
 
-void lectureTexte(char* nomFichier, listeChar* liste){
-	FILE* file;
+void lectureTexte(char *nomFichier, listeChar *liste)
+{
+	FILE *file;
 	char val;
 	int compteur = 0;
 
+	Caractere *position = liste->premier;
 
-	Caractere* position = liste->premier;
-
-	if(!existeFichier(nomFichier)){
+	if (!existeFichier(nomFichier))
+	{
 		printf("\n/Le fichier %s n existe pas./\n", nomFichier);
 		exit(-1);
-	}else{
-		if((file=fopen(nomFichier, "r"))==NULL){
-			printf("Erreur lors de l ouverture du fichier %s.",nomFichier);
+	}
+	else
+	{
+		if ((file = fopen(nomFichier, "r")) == NULL)
+		{
+			printf("Erreur lors de l ouverture du fichier %s.", nomFichier);
 			exit(-1);
-		}else{
+		}
+		else
+		{
 			rewind(file);
-			while((val = fgetc(file))!=EOF){
+			while ((val = fgetc(file)) != EOF)
+			{
 				position = liste->premier;
 				compteur = 0;
-				if(position == NULL){
-					ajoutTabFin(val,nomFichier, liste);
-				}else{
-					//printf("\nval=%c\n",val);
-					if(position->carac != val){
-						while(position->suivant != NULL){
+				if (position == NULL)
+				{
+					ajoutTabFin(val, nomFichier, liste);
+				}
+				else
+				{
+					// printf("\nval=%c\n",val);
+					if (position->carac != val)
+					{
+						while (position->suivant != NULL)
+						{
 							position = position->suivant;
-							if(position->carac == val){
+							if (position->carac == val)
+							{
 								compteur++;
 							}
 						}
-						//printf("\ncompteur = %d\n", compteur);
-						if(compteur == 0){
-							//printf("\nval=%c\n",val);
+						// printf("\ncompteur = %d\n", compteur);
+						if (compteur == 0)
+						{
+							// printf("\nval=%c\n",val);
 							position->suivant = ajoutTab(val, nomFichier, liste);
 						}
 					}
 				}
 			}
-			fclose(file); 
+			fclose(file);
 		}
 	}
 }
 
-void afficherTab(listeChar* liste){
+void afficherTab(listeChar *liste)
+{
 
-	Caractere* current = liste->premier;
+	Caractere *current = liste->premier;
 
-	while(current!= NULL){
-		printf("\ncar=%c;freq=%d\n",current->carac, current->frequence);
+	while (current != NULL)
+	{
+		printf("\ncar=%c;freq=%d\n", current->carac, current->frequence);
 		current = current->suivant;
 	}
 }
 
-int main(int argc, char *argv[]){
+listeChar *sorting(listeChar *liste)
+{
+	/*
+
+	*/
+}
+
+int main(int argc, char *argv[])
+{
 
 	tabCarac = initialisationListe();
 
 	printf("\n%s\n", argv[1]);
-	
-	switch(argv[1][1]){
-		case'c':
-			printf("\nCompression\n");
-			printf("\n%s\n",argv[2]);
-			lectureTexte(argv[2], tabCarac);
-			//printf("\ntest2\n");
-			//afficherTab(tabCarac);
-			break;
-		case'd':
-			printf("\nDecomp resion\n");
-			printf("\n%s\n",argv[2]);
-			break;
-		default:
-			printf("\nErreur de saisi. Entrer en parametre c ou d.\n");
+
+	switch (argv[1][1])
+	{
+	case 'c':
+		printf("\nCompression\n");
+		printf("\n%s\n", argv[2]);
+		lectureTexte(argv[2], tabCarac);
+		// printf("\ntest2\n");
+		// afficherTab(tabCarac);
+		break;
+	case 'd':
+		printf("\nDecomp resion\n");
+		printf("\n%s\n", argv[2]);
+		break;
+	default:
+		printf("\nErreur de saisi. Entrer en parametre c ou d.\n");
 	}
-	
+
 	return 0;
 }
